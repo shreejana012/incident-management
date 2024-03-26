1a3ce08e378d49dd26eb197159e7a86d7b174faf
Horace Yau <horace701@hotmail.com>
2024-03-25 23:12:06 -0400

    client/vite.config.js: update port to 8081

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-const { PORT = 3000} = process.env;
+const { PORT = 8081} = process.env;
 
 export default defineConfig({
  plugins: [react()],
