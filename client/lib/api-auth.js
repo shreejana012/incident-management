fa3f83e314cef2fa358a9575e0505aac91c404bf
shrijanapoudel <shrijanapoudel@whitehatengineering.com>
2024-04-08 20:34:02 -0400

    Testing deployment

--- client/lib/api-auth.js
+++ client/lib/api-auth.js
@@ -1,37 +1,36 @@
-const signin = async (user) => { 
+const signin = async (user) => {
     try {
-        let response = await fetch('/auth/signin/', { 
-        //let response = await fetch('https://incident-management.onrender.com/auth/signin/', { 
-        //let response = await fetch('https://incident-management.onrender.com/', { 
-        //let response = await fetch('https://cors-anywhere.herokuapp.com/https://incident-management.onrender.com/auth/signin/', { 
+        let response = await fetch('https://incident-management-gt87.onrender.com/auth/signin/', {
+            //let response = await fetch('https://incident-management.onrender.com/auth/signin/', { 
+            //let response = await fetch('https://incident-management.onrender.com/', { 
+            //let response = await fetch('https://cors-anywhere.herokuapp.com/https://incident-management.onrender.com/auth/signin/', { 
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
-                'Content-Type': 'application/json' 
+                'Content-Type': 'application/json'
             },
-            credentials: 'include', 
+            credentials: 'include',
             body: JSON.stringify(user)
         })
-    return await response.json() 
-    } catch(err) {
-        console.log(err) 
+        return await response.json()
+    } catch (err) {
+        console.log(err)
     }
-    }
+}
 
-const signout = async () => { 
+const signout = async () => {
     try {
-        let response = await fetch('/auth/signout/', { method: 'GET' }) 
+        let response = await fetch('/auth/signout/', { method: 'GET' })
         /*
         let response = await fetch('https://incident-management.onrender.com/auth/signout/', { 
             method: 'GET' 
         })
-        */ 
+        */
         return await response.json()
-    } catch(err) { 
+    } catch (err) {
         console.log(err)
-    } 
+    }
 }
-    
+
 export { signin, signout }
-    
-    
+
